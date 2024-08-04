0f39de3aa11a8c2d3fe8c8d9c92dc6760f27deda
Noor Ahmad Bashery <noorahmadbashery@gmail.com>
2024-08-04 08:34:46 +0430

    Travel List Complete

--- src/App.js
+++ src/App.js
@@ -1,46 +1,91 @@
 import { useState } from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "charger", quantity: 2, packed: true },
+];
+
 export default function App() {
   return (
-    <div>
-      <Step />
-      <Counter />
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Status />
     </div>
   );
 }
 
-function Step() {
-  return <div>hh</div>;
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-  const[step,setStep] =useState(1);
-  const date = new Date("june 21 2027");
-  date.setDate(date.getDate() + count);
+function Logo() {
   return (
-    <>
-      <button onClick={() => setStep((s) => s - 1)}>-</button>
-      <div>Step: {step}</div>
-      <button onClick={() => setStep((s) => s + 1)}>+</button>
-<p>
+    <div>
+      <h1>🌴 Far Away 💼</h1>
+    </div>
+  );
+}
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState();
 
-      <button onClick={() => setCount((s) => s - step)}>-</button>
-      <div>Count: {count}</div>
-      <button onClick={() => setCount((s) => s + step)}>+</button>
-</p>
-      <p>
-        <span>
-          <span>
-            {count === 0
-              ? "Today is "
-              : count > 0
-              ? `${count}  days from today is `
-              : `${Math.abs(count)} days ago was`}
-          </span>
-          {date.toDateString()}
-        </span>
-      </p>
-    </>
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id:  Date.now(),
+    };
+    console.log(newItem);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for you trip</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>➕</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Status() {
+  return (
+    <footer className="stats">
+      <em>💼You have X items on your list, and you already packed X(X%)</em>
+    </footer>
   );
 }
